35c360a8ed20706896583125641d74339373f117
kkitko808 <dento85@tutamail.com>
2020-03-11 13:24:07 -0500

    Working burgers app

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO cats (name, sleepy) VALUES ('Veggie Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Bison Burger', true);
+INSERT INTO cats (name, sleepy) VALUES ('Cheese Burger', false);
